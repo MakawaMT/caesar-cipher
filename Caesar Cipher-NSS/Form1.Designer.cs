b2c3bf79f38fcc35631091fb5493058408e2c451
MakawaMT <mtmakawa@gmail.com>
2016-06-23 22:02:55 +0200

    Initial Commit

---
+++ Caesar Cipher-NSS/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Caesar_Cipher_NSS
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(29, 91);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(348, 337);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(573, 91);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(348, 337);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(427, 165);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(98, 44);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(427, 237);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(98, 44);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(427, 309);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(98, 44);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(570, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(310, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(332, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "CAESAR CIPHER PROGRAM";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 463);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Caesar Cipher";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Caesar Cipher-NSS/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic; //use this reference to refer to the InputBox only found in Visual Basic
+
+namespace Caesar_Cipher_NSS
+{
+    /*
+     * This program emulates the Caesar cipher.
+     * It accepts alphabetical characters,spaces, numbers and fullstops. Does not cater for special characters.
+     * 
+     * Program is for Assignment 1 of Network Systems Security
+     * 28/03/2015
+     * Group 5
+     * Amadhila M
+     * M. T Makawa
+     * Simate M
+     *
+     * 
+     * This program is subject to the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0.html)
+     * 
+     */
+    public partial class frmMain : Form
+    {
+        //characters used for shifting
+        private String[] smallLetters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        private String[] capLetters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            //variables to store plaintext, user key and calculated key
+            String plainText = "";
+            int privateKey, temp;
+
+            try
+            {
+
+                //ask user to enter an integer key to be used to encrypt the data
+                Int32.TryParse(Interaction.InputBox("Input private key here", "Private Key", "0"), out privateKey);
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Key conversion error");//inform user of invalid key
+                return; //stop further execution
+            }
+
+            //get user input text and check if it's empty
+            plainText = txtInput.Text;
+            
+            if(plainText=="")
+            {
+                //inform user that there is no text to encrypt and stop execution
+                MessageBox.Show("No text to encrypt");
+                return;
+            }
+
+            //calculate program key to encrypt with
+
+            temp = privateKey % 26;//every key is modulus of 26 whether in the thousands or not
+
+            //reject multiples of 26 because they will end up being zero
+            if (temp == 0)
+            {
+                MessageBox.Show("Not a valid shift key","Error: Bed Shift Key",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+
+            //encrypt and show the output
+            txtOutput.Text = CaesarEncrypt(plainText, privateKey);
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            String cipherText;
+            int privateKey, temp;
+
+            try
+            {
+                //ask user to enter private key
+                Int32.TryParse(Interaction.InputBox("Enter private key", "Private Key", "0"), out privateKey);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Key Conversion Error");
+                return;//stop execution
+            }
+
+            cipherText = txtInput.Text;
+
+            if (cipherText == "") 
+            {
+                MessageBox.Show("No text to encrypt");
+                return;
+            }
+
+            temp = privateKey % 26;
+
+            if (temp == 0)
+            {
+                MessageBox.Show("Invalid private key used", "Error: Bad shift key", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            txtOutput.Text = CaesarDecrypt(cipherText, privateKey);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();//close the program
+        }
+
+        private String CaesarEncrypt(String plainText, int privateKey)
+        {
+            
+            
+            String cipherText = "",tempChar="";
+
+            #region Encrypt
+
+            try
+            {
+                for (int x = 0; x < plainText.Length; x++)
+                {
+                    //take one character at a time
+                    tempChar = plainText.Substring(x, 1);
+
+                    //if character is space, number or period do not encrypt just append
+                    if (tempChar.Equals(" ") || tempChar.All(char.IsDigit) || tempChar.Equals(".")) 
+                    {
+                        cipherText = cipherText + tempChar;
+                        continue;//move on to the next character
+                    }
+                   
+
+                    #region Loop4characters
+                    //loop for characters throughout the alphabet to find the correct one
+                    for(int y=0;y<=25;y++)
+                    {
+                        if(smallLetters[y].Equals(tempChar))
+                        {
+                            cipherText = cipherText + smallLetters[y + privateKey];//apply shift
+                            break;//move on to next character. use break since this is inner loop
+                            
+                        }
+                        if(capLetters[y].Equals(tempChar))
+                        {
+                            cipherText = cipherText + capLetters[y + privateKey];//apply shift
+                            break;//move on to next character. use break since this is inner loop
+                        }
+                    }
+                    #endregion Loop4characters
+
+                }
+
+            }
+            catch (Exception ex) 
+            { 
+                //display error details and break execution
+                MessageBox.Show(ex.Message, "Error Encrypting", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+
+
+            #endregion Encrypt
+
+            return cipherText;
+        }
+
+        private String CaesarDecrypt(String cipherText,int privateKey)
+        {
+            
+            String plainText = "",tempChar="";
+
+         #region Decrypt
+            try
+            {
+                for (int x = 0; x < cipherText.Length; x++)
+                {   
+                    //take one character at a time
+                    tempChar = cipherText.Substring(x, 1);
+
+                    //if character is space, period or number do not encrypt then just append
+                    if (tempChar.Equals(" ") || tempChar.All(char.IsDigit) || tempChar.Equals("."))
+                    {
+                        plainText = plainText + tempChar;
+                        continue;//move on to the next character
+                       
+                    }
+                  
+
+                    #region Loop4characters
+                    for(int y=51;y>=26;y--)//loop backwards to avoid IndexOutOfBounds error
+                    {
+
+                        if (smallLetters[y].Equals(tempChar))
+                        {
+                            plainText = plainText + smallLetters[y - privateKey];
+                            break;//move on to next character . use break since this is inner loop
+                        }
+                        else if (capLetters[y].Equals(tempChar))
+                        {
+                            plainText = plainText + capLetters[y - privateKey];
+                            break;//move on to next character. use break since this is inner loop
+                        }
+                    }
+                    #endregion Loop4characters
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error Decrypting",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return null;
+            }
+         #endregion Decrypt
+            //return the decrypted text
+            return plainText;
+        }
+    }
+}
